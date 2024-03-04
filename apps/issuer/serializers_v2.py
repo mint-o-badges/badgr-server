e7361cf9c14fe65e4c1acf085020448b6f25bad7
jona159 <raabejonas@yahoo.de>
2024-03-04 13:52:21 +0100

    remove unnecessary extra space

--- apps/issuer/serializers_v2.py
+++ apps/issuer/serializers_v2.py
@@ -75,7 +75,7 @@ class IssuerSerializerV2(DetailSerializerV2, OriginalJsonSerializerMixin):
     createdBy = EntityRelatedFieldV2(source='cached_creator', queryset=BadgeUser.cached, required=False)
     name = StripTagsCharField(max_length=1024)
     image = ValidImageField(required=False, use_public=True, source='*')
-    email = serializers.EmailField(max_length=255, required=True) 
+    email = serializers.EmailField(max_length=255, required=True)
     description = StripTagsCharField(max_length=16384, required=False)
     url = serializers.URLField(max_length=1024, required=True)
     staff = IssuerStaffSerializerV2(many=True, source='staff_items', required=False)
