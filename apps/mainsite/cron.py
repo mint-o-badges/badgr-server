9c808dea432ccce3c22ba6b2f8f7e70dad9fd901
jona159 <raabejonas@yahoo.de>
2024-06-26 15:27:57 +0200

    fix cron

--- apps/mainsite/cron.py
+++ apps/mainsite/cron.py
@@ -5,13 +5,7 @@ class MyCronJob(CronJobBase):
     RUN_EVERY_MINS = 1 
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'mainsite.my_cron_job'    
+    code = 'mainsite.MyCronJob'    
 
     def do(self):
-        clear_expired   
-
-
-# def daily_cron():
-#     print("Running daily cron")
-#     clear_expired()
-#     print("Finished daily cron")
+        clear_expired()   
