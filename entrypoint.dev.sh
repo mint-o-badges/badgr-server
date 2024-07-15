f3567e388458fe12c06b0dd12b52154d1892044e
jona159 <raabejonas@yahoo.de>
2024-07-15 12:16:34 +0200

    fix: old configuration

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-# Start the cron service
-service cron start
+/badgr_server/manage.py migrate
 
-# add cronjobs
 /badgr_server/manage.py crontab remove
 /badgr_server/manage.py crontab add
 
-# apply migrations
-/badgr_server/manage.py migrate
+/badgr_server/manage.py extract_crontab
+
+supercronic crontab &
 
 # Start the Django server
 exec /badgr_server/manage.py runserver 0.0.0.0:8000
