d3db20b9ac1bb508afc6f2c7f6635df63af1e6d4
jona159 <raabejonas@yahoo.de>
2024-10-18 10:00:10 +0200

    feat: use only supercronic

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -5,6 +5,8 @@ set -e
 /badgr_server/manage.py migrate
 /badgr_server/manage.py collectstatic --noinput
 
+supercronic /etc/cron.d/crontab &
+
 # Start the Django server
 exec /badgr_server/manage.py runserver 0.0.0.0:8000
 
