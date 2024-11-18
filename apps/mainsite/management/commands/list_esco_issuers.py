703d2f3b87cd550e7d8297166387956825ff3ca9
jona159 <raabejonas@yahoo.de>
2024-11-18 10:49:47 +0100

    fix: print set

--- apps/mainsite/management/commands/list_esco_issuers.py
+++ apps/mainsite/management/commands/list_esco_issuers.py
@@ -6,6 +6,7 @@ import os
 class Command(BaseCommand):
     def handle(self, *args, **options):
         issuerArray = []
+        badgesArray = []
         badgeclasses = BadgeClass.objects.all()
 
         file_path = os.path.join(os.getcwd(), 'issuers.txt')
@@ -13,7 +14,6 @@ class Command(BaseCommand):
             with open(file_path, 'w') as f:
                 for badgeclass in badgeclasses:
                     extensions = badgeclass.get_extensions_manager()
-
                     competency_extension = extensions.filter(name='extensions:CompetencyExtension').first()
                     if competency_extension is not None:
                         competency_json = competency_extension.original_json
@@ -21,9 +21,13 @@ class Command(BaseCommand):
                         for item in competency_dict: 
                             escoID = item.get('escoID')
                             if escoID is not None and escoID != '': 
+                                print("competency_dict: ", competency_dict)
                                 issuer = badgeclass.issuer
-                                issuerArray.append(issuer)
-                                f.write(f"Name: {issuer.name}, Creator {issuer.cached_creator.email}  \n")
+                                issuerArray.append(issuer.name)
+                                badgesArray.append(badgeclass.entity_id)
+                issuerSet = set(issuerArray)
+                badgesSet = set(badgesArray)
+                f.write(f"IssuerName: {issuerSet}, BadgeClass {badgesSet}  \n")                                
 
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'An error occurred: {str(e)}'))
