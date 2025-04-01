83f211a7eb3aee7b979cac1d95a4f88226e558d9
jona159 <raabejonas@yahoo.de>
2025-04-01 16:03:35 +0200

    feat: add badge request email and cronjob

---
+++ apps/mainsite/management/commands/send_badgerequest_mails.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from issuer.models import RequestedBadge
+from allauth.account.adapter import get_adapter
+
+class Command(BaseCommand):
+    help = 'Send mail to issuer staff when badges were requested via qr code that day'
+
+    def handle(self, *args, **kwargs):
+
+        requested_badges = RequestedBadge.objects.filter()
+
+        for badge in requested_badges: 
+            for member in badge.qrcode.issuer.cached_issuerstaff():
+                if badge.qrcode.notifications: 
+                    ctx = {
+                        'badge_name': badge.badgeclass.name,
+                        'call_to_action_label': "Anfrage bestätigen"
+                    }
+                    get_adapter().send_mail('account/email/email_badge_request',
+                                    member.cached_user.email, ctx)
+            
+
+        
+        self.stdout.write(
+            self.style.SUCCESS(f'Successfully sent emails')
+        )
