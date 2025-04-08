3622564fdad75a3a0917ab2f59569fd693067d67
David Scheidt <david.scheidt@opensenselab.org>
2025-04-08 13:39:28 +0200

    docs: add documentation

--- apps/mainsite/management/commands/send_badgerequest_mails.py
+++ apps/mainsite/management/commands/send_badgerequest_mails.py
@@ -1,26 +1,28 @@
+from allauth.account.adapter import get_adapter
 from django.core.management.base import BaseCommand
 from issuer.models import RequestedBadge
-from allauth.account.adapter import get_adapter
+
 
 class Command(BaseCommand):
-    help = 'Send mail to issuer staff when badges were requested via qr code that day'
+    """Send mail to issuer staff when badges were requested via qr code"""
+
+    help = "Send mail to issuer staff when badges were requested via qr code that day"
 
     def handle(self, *args, **kwargs):
 
         requested_badges = RequestedBadge.objects.filter()
 
-        for badge in requested_badges: 
+        for badge in requested_badges:
             for member in badge.qrcode.issuer.cached_issuerstaff():
-                if badge.qrcode.notifications: 
+                if badge.qrcode.notifications:
                     ctx = {
-                        'badge_name': badge.badgeclass.name,
-                        'call_to_action_label': "Anfrage bestätigen"
+                        "badge_name": badge.badgeclass.name,
+                        "call_to_action_label": "Anfrage bestätigen",
                     }
-                    get_adapter().send_mail('account/email/email_badge_request',
-                                    member.cached_user.email, ctx)
-            
+                    get_adapter().send_mail(
+                        "account/email/email_badge_request",
+                        member.cached_user.email,
+                        ctx,
+                    )
 
-        
-        self.stdout.write(
-            self.style.SUCCESS(f'Successfully sent emails')
-        )
+        self.stdout.write(self.style.SUCCESS("Successfully sent emails"))
