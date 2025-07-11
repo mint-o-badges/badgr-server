f323a533c79da48ffee574ea9e6e41839211d9e5
jona159 <65068389+jona159@users.noreply.github.com>
2025-07-11 15:38:58 +0200

    Feat/institution verification mail (#477)
    * feat: wip new verification mail
    
    * feat: add thumbnail, redo email
    
    * fix: use f-string
    
    ---------
    
    Co-authored-by: David Scheidt <david.scheidt@opensenselab.org>

--- apps/issuer/models.py
+++ apps/issuer/models.py
@@ -413,6 +413,7 @@ class Issuer(
                     "issuer_url": self.url,
                     "issuer_email": self.email,
                     "badgr_app": badgr_app,
+                    "thumbnail_url": f"{settings.STATIC_URL}/images/badgeCreateThumbnail.png",
                 }
             except KeyError as e:
                 # A property isn't stored right in json
