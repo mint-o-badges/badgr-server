2f03462ef40415c3e5a4322f5e01f8f565a4ab4d
Stefan Trautvetter <stefan.trautvetter@esirion.de>
2025-08-11 14:22:47 +0200

    feat: update webcomponent, use OEBWebComponentSettings, fixes

--- apps/mainsite/views_iframes.py
+++ apps/mainsite/views_iframes.py
@@ -1,16 +1,12 @@
 import json
 
-from allauth.account.admin import EmailAddress
 from django.conf import settings
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
 from lti_tool.views import csrf_exempt
 
-from badgeuser.models import BadgeUser
-from backpack.utils import get_skills_tree
 from mainsite.models import IframeUrl
-from issuer.models import BadgeInstance
 
 
 @xframe_options_exempt
@@ -20,8 +16,7 @@ def iframe(request, *args, **kwargs):
     try:
         iframe = IframeUrl.objects.get(id=iframe_uuid)
         # get from db and delete to create "single-use" urls
-        # TODO: might be better to change to a time based solution,
-        # cleanup of old entries will be needed either way
+        # TODO: might be better to change to a time based solution
         iframe.delete()
     except IframeUrl.DoesNotExist:
         return HttpResponseNotFound()
@@ -43,4 +38,8 @@ def iframe(request, *args, **kwargs):
 
 def iframe_profile(request, skills, language):
     skill_json = json.dumps(skills, ensure_ascii=False)
-    return render(request, "lti/profile/index.html", context={"skill_json": skill_json})
+    return render(
+        request,
+        "iframes/profile/index.html",
+        context={"skill_json": skill_json, "language": language},
+    )
