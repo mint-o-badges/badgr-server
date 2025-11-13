1c7b994c609d2de9076e5c692de03c976b90dda7
jona159 <raabejonas@yahoo.de>
2025-11-13 07:28:56 +0100

    fix: rm unused imports

--- apps/issuer/api.py
+++ apps/issuer/api.py
@@ -88,7 +88,6 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
-    HTTP_201_CREATED,
     HTTP_204_NO_CONTENT,
     HTTP_400_BAD_REQUEST,
     HTTP_404_NOT_FOUND,
