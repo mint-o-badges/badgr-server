1ac84e08bf4efe728c44a8e1bcedb716eca1c768
jona159 <raabejonas@yahoo.de>
2025-11-14 07:01:29 +0100

    feat: migrate urls to re_path

--- apps/mainsite/urls.py
+++ apps/mainsite/urls.py
@@ -48,7 +48,7 @@ from mainsite.views_lti import (
 
 from django.apps import apps
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.urls import path
 
 from mainsite.admin import badgr_admin
@@ -79,57 +79,57 @@ def django2_include(three_tuple_urlconf):
 
 urlpatterns = [
     # Backup URLs in case the server isn't serving these directly
-    url(
+    re_path(
         r"^favicon\.png[/]?$",
         RedirectView.as_view(
             url="%simages/favicon.png" % settings.STATIC_URL, permanent=True
         ),
     ),
-    url(
+    re_path(
         r"^favicon\.ico[/]?$",
         RedirectView.as_view(
             url="%simages/favicon.png" % settings.STATIC_URL, permanent=True
         ),
     ),
-    url(
+    re_path(
         r"^robots\.txt$",
         RedirectView.as_view(url="%srobots.txt" % settings.STATIC_URL, permanent=True),
     ),
     # legacy logo url redirect
-    url(
+    re_path(
         r"^static/images/header-logo-120.png$",
         RedirectView.as_view(
             url="{}images/logo.png".format(settings.STATIC_URL), permanent=True
         ),
     ),
     # Apple app universal URL endpoint
-    url(
+    re_path(
         r"^apple-app-site-association",
         AppleAppSiteAssociation.as_view(),
         name="apple-app-site-association",
     ),
     # OAuth2 provider URLs
-    url(
+    re_path(
         r"^o/authorize/?$", AuthorizationApiView.as_view(), name="oauth2_api_authorize"
     ),
-    url(
+    re_path(
         r"^o/token/?$",
         ensure_csrf_cookie(TokenView.as_view()),
         name="oauth2_provider_token",
     ),
-    url(
+    re_path(
         r"^o/revoke_token/?$",
         RevokeTokenView.as_view(),
         name="oauth2_provider_revoke_token",
     ),
-    url(r"^o/code/?$", AuthCodeExchange.as_view(), name="oauth2_code_exchange"),
-    url(
+    re_path(r"^o/code/?$", AuthCodeExchange.as_view(), name="oauth2_code_exchange"),
+    re_path(
         r"^o/register/?$",
         RegisterApiView.as_view(),
         kwargs={"version": "rfc7591"},
         name="oauth2_api_register",
     ),
-    url(
+    re_path(
         r"^o/publicregister/?$",
         PublicRegisterApiView.as_view(),
         kwargs={"version": "rfc7591"},
@@ -137,113 +137,121 @@ urlpatterns = [
     ),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
     # Badge Connect URLs
-    url(
+    re_path(
         r"^bcv1/manifest/(?P<domain>[^/]+)$",
         BadgeConnectManifestView.as_view(),
         name="badge_connect_manifest",
     ),
-    url(
+    re_path(
         r"^\.well-known/badgeconnect.json$",
         BadgeConnectManifestRedirectView.as_view(),
         name="default_bc_manifest_redirect",
     ),
-    url(r"^bcv1/", include("backpack.badge_connect_urls"), kwargs={"version": "bcv1"}),
+    re_path(
+        r"^bcv1/", include("backpack.badge_connect_urls"), kwargs={"version": "bcv1"}
+    ),
     # Home
-    url(r"^$", info_view, name="index"),
-    url(
+    re_path(r"^$", info_view, name="index"),
+    re_path(
         r"^accounts/login/$", RedirectToUiLogin.as_view(), name="legacy_login_redirect"
     ),
     # Admin URLs
-    url(
+    re_path(
         r"^staff/sidewide-actions$",
         SitewideActionFormView.as_view(),
         name="badgr_admin_sitewide_actions",
     ),
-    url(r"^staff/", django2_include(badgr_admin.urls)),
+    re_path(r"^staff/", django2_include(badgr_admin.urls)),
     # Service health endpoint
-    url(r"^health", include("health.urls")),
+    re_path(r"^health", include("health.urls")),
     # Swagger Docs
     #
     # api docs
     #
-    url(
+    re_path(
         r"^docs/oauth2/authorize$",
         DocsAuthorizeRedirect.as_view(),
         name="docs_authorize_redirect",
     ),
-    url(
+    re_path(
         r"^docs/?$", RedirectView.as_view(url="/docs/v2/", permanent=True)
     ),  # default redirect to /v2/
-    url(r"^docs/", include("apispec_drf.urls")),
+    re_path(r"^docs/", include("apispec_drf.urls")),
     # unversioned public endpoints
-    url(
+    re_path(
         r"^unsubscribe/(?P<email_encoded>[^/]+)/(?P<expiration>[^/]+)/(?P<signature>[^/]+)",
         email_unsubscribe,
         name="unsubscribe",
     ),
-    url(r"^public/", include("issuer.public_api_urls"), kwargs={"version": "v2"}),
+    re_path(r"^public/", include("issuer.public_api_urls"), kwargs={"version": "v2"}),
     # legacy share redirects
-    url(r"", include("backpack.share_urls")),
+    re_path(r"", include("backpack.share_urls")),
     # Legacy Auth Token Endpoint: Deprecated and logged
-    url(r"^api-auth/token$", LegacyLoginAndObtainAuthToken.as_view()),
+    re_path(r"^api-auth/token$", LegacyLoginAndObtainAuthToken.as_view()),
     # Social Auth (oAuth2 and SAML)
-    url(r"^account/", include("badgrsocialauth.urls")),
+    re_path(r"^account/", include("badgrsocialauth.urls")),
     # v1 API endpoints
-    url(r"^v1/user/", include("badgeuser.v1_api_urls"), kwargs={"version": "v1"}),
-    url(r"^v1/user/", include("badgrsocialauth.v1_api_urls"), kwargs={"version": "v1"}),
-    url(r"^v1/issuer/", include("issuer.v1_api_urls"), kwargs={"version": "v1"}),
-    url(r"^v1/earner/", include("backpack.v1_api_urls"), kwargs={"version": "v1"}),
+    re_path(r"^v1/user/", include("badgeuser.v1_api_urls"), kwargs={"version": "v1"}),
+    re_path(
+        r"^v1/user/", include("badgrsocialauth.v1_api_urls"), kwargs={"version": "v1"}
+    ),
+    re_path(r"^v1/issuer/", include("issuer.v1_api_urls"), kwargs={"version": "v1"}),
+    re_path(r"^v1/earner/", include("backpack.v1_api_urls"), kwargs={"version": "v1"}),
     # v2 API endpoints
-    url(r"^v2/", include("issuer.v2_api_urls"), kwargs={"version": "v2"}),
-    url(r"^v2/", include("badgeuser.v2_api_urls"), kwargs={"version": "v2"}),
-    url(r"^v2/", include("badgrsocialauth.v2_api_urls"), kwargs={"version": "v2"}),
-    url(r"^v2/backpack/", include("backpack.v2_api_urls"), kwargs={"version": "v2"}),
-    url(r"^upload", upload, name="image_upload"),
-    url(
+    re_path(r"^v2/", include("issuer.v2_api_urls"), kwargs={"version": "v2"}),
+    re_path(r"^v2/", include("badgeuser.v2_api_urls"), kwargs={"version": "v2"}),
+    re_path(r"^v2/", include("badgrsocialauth.v2_api_urls"), kwargs={"version": "v2"}),
+    re_path(
+        r"^v2/backpack/", include("backpack.v2_api_urls"), kwargs={"version": "v2"}
+    ),
+    re_path(r"^upload", upload, name="image_upload"),
+    re_path(
         r"^nounproject/(?P<searchterm>[^/]+)/(?P<page>[^/]+)$",
         nounproject,
         name="nounproject",
     ),
-    url(r"^aiskills/$", aiskills, name="aiskills"),
-    url(r"^aiskills-keywords/$", aiskills_keywords, name="aiskills_keywords"),
-    url(r"^request-badge/(?P<qrCodeId>[^/]+)$", requestBadge, name="request-badge"),
-    url(r"^get-server-timestamp", getTimestamp, name="get-server-timestamp"),
-    url(
+    re_path(r"^aiskills/$", aiskills, name="aiskills"),
+    re_path(r"^aiskills-keywords/$", aiskills_keywords, name="aiskills_keywords"),
+    re_path(r"^request-badge/(?P<qrCodeId>[^/]+)$", requestBadge, name="request-badge"),
+    re_path(r"^get-server-timestamp", getTimestamp, name="get-server-timestamp"),
+    re_path(
         r"^deleteBadgeRequest/(?P<requestId>[^/]+)$",
         deleteBadgeRequest,
         name="delete-badge-request",
     ),
-    url(
+    re_path(
         r"^download-qrcode/(?P<qrCodeId>[^/]+)/(?P<badgeSlug>[^/]+)$",
         downloadQrCode,
         name="download-qrcode",
     ),
-    url(
+    re_path(
         r"^badgeRequests/(?P<badgeSlug>[^/]+)$",
         badgeRequestsByBadgeClass,
         name="badge-requests-by-badgeclass",
     ),
-    url(r"^v3/", include("issuer.v3_api_urls"), kwargs={"version": "v3"}),
-    url(r"^v3/backpack/", include("backpack.v3_api_urls"), kwargs={"version": "v3"}),
-    url(r"^v3/issuer/", include("issuer.v3_api_urls"), kwargs={"version": "v3"}),
-    url(r"^v3/user/", include("badgeuser.v3_api_urls"), kwargs={"version": "v3"}),
-    url(r"^v3/admin/users", AdminUser.as_view({"get": "list"})),
-    url(r"^v3/admin/issuers", AdminIssuer.as_view({"get": "list"})),
+    re_path(r"^v3/", include("issuer.v3_api_urls"), kwargs={"version": "v3"}),
+    re_path(
+        r"^v3/backpack/", include("backpack.v3_api_urls"), kwargs={"version": "v3"}
+    ),
+    re_path(r"^v3/issuer/", include("issuer.v3_api_urls"), kwargs={"version": "v3"}),
+    re_path(r"^v3/user/", include("badgeuser.v3_api_urls"), kwargs={"version": "v3"}),
+    re_path(r"^v3/admin/users", AdminUser.as_view({"get": "list"})),
+    re_path(r"^v3/admin/issuers", AdminIssuer.as_view({"get": "list"})),
     # meinBildungsraum OIDC connection
     path("oidc/", include("mozilla_django_oidc.urls")),
-    url(
+    re_path(
         r"^oidcview/logoutRedirect/",
         OidcView.oidcLogoutRedirect,
         name="oidcLogoutRedirect",
     ),
-    url(r"^altcha", createCaptchaChallenge, name="create_captcha_challenge"),
-    url(r"^cms/menu/list/?$", cms_api_menu_list, name="cms_api_menu_list"),
-    url(r"^cms/post/list/?$", cms_api_post_list, name="cms_api_post_list"),
-    url(r"^cms/page/slug/?$", cms_api_page_details, name="cms_api_page_details"),
-    url(r"^cms/post/slug/?$", cms_api_post_details, name="cms_api_post_details"),
-    url(r"^cms/style/?$", cms_api_style, name="cms_api_style"),
-    url(r"^cms/script/?$", cms_api_script, name="cms_api_script"),
-    url(r"^cms/(?P<path>.+)$", call_cms_api, name="call_cms_api"),
+    re_path(r"^altcha", createCaptchaChallenge, name="create_captcha_challenge"),
+    re_path(r"^cms/menu/list/?$", cms_api_menu_list, name="cms_api_menu_list"),
+    re_path(r"^cms/post/list/?$", cms_api_post_list, name="cms_api_post_list"),
+    re_path(r"^cms/page/slug/?$", cms_api_page_details, name="cms_api_page_details"),
+    re_path(r"^cms/post/slug/?$", cms_api_post_details, name="cms_api_post_details"),
+    re_path(r"^cms/style/?$", cms_api_style, name="cms_api_style"),
+    re_path(r"^cms/script/?$", cms_api_script, name="cms_api_script"),
+    re_path(r"^cms/(?P<path>.+)$", call_cms_api, name="call_cms_api"),
     # iframes
     path("iframes/<uuid:iframe_uuid>/", iframe, name="iframe"),
     # LTI
@@ -264,8 +272,8 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Test URLs to allow you to see these pages while DEBUG is True
 if getattr(settings, "DEBUG_ERRORS", False):
     urlpatterns = [
-        url(r"^error/404/$", error404, name="404"),
-        url(r"^error/500/$", error500, name="500"),
+        re_path(r"^error/404/$", error404, name="404"),
+        re_path(r"^error/500/$", error500, name="500"),
     ] + urlpatterns
 
 # If DEBUG_MEDIA is set, have django serve anything in MEDIA_ROOT at MEDIA_URL
@@ -274,7 +282,7 @@ if getattr(settings, "DEBUG_MEDIA", True):
 
     media_url = getattr(settings, "MEDIA_URL", "/media/").lstrip("/")
     urlpatterns = [
-        url(
+        re_path(
             r"^media/(?P<path>.*)$",
             static_serve,
             {"document_root": settings.MEDIA_ROOT},
@@ -291,7 +299,7 @@ if getattr(settings, "DEBUG_STATIC", True):
     )
     static_url = static_url.lstrip("/")
     urlpatterns = [
-        url(
+        re_path(
             r"^%s(?P<path>.*)" % (static_url,),
             staticfiles_serve,
             kwargs={
@@ -305,7 +313,7 @@ if getattr(settings, "DEBUG", True) or getattr(
     settings, "SERVE_PATTERN_LIBRARY", False
 ):
     urlpatterns = [
-        url(
+        re_path(
             r"^component-library$",
             TemplateView.as_view(template_name="component-library.html"),
             name="component-library",
@@ -318,7 +326,7 @@ if settings.DEBUG and apps.is_installed("debug_toolbar"):
         import debug_toolbar
 
         urlpatterns = urlpatterns + [
-            url(r"^__debug__/", include(debug_toolbar.urls)),
+            re_path(r"^__debug__/", include(debug_toolbar.urls)),
         ]
     except ImportError:
         pass
