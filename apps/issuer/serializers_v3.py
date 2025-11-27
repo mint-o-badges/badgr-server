6877e1c70fae9495d0868fe421a3f1cdb7b7ada3
jona159 <raabejonas@yahoo.de>
2025-11-27 12:36:22 +0100

    fix: rm unused imports

--- apps/issuer/serializers_v3.py
+++ apps/issuer/serializers_v3.py
@@ -1,11 +1,5 @@
-from collections import OrderedDict
-
-import pytz
-
-from apps.mainsite.serializers import DateTimeWithUtcZAtEndField
 from entity.serializers import DetailSerializerV2
-from issuer.models import BadgeClass, BadgeInstance, Issuer
-from rest_framework import serializers
+from issuer.models import BadgeClass
 
 
 # only for apispec for now
