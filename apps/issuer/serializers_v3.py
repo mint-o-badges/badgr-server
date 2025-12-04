c41370a61c71c5b427e6d0191e4912266b1ca457
David Scheidt <david.scheidt@opensenselab.org>
2025-12-04 12:13:53 +0100

    feat: add serializers and require json to use iframe endpoints

--- apps/issuer/serializers_v3.py
+++ apps/issuer/serializers_v3.py
@@ -1,10 +1,28 @@
+from rest_framework import serializers
 from entity.serializers import DetailSerializerV2
 from issuer.models import BadgeClass
-
-
-# only for apispec for now
 
 
 class BadgeClassSerializerV3(DetailSerializerV2):
     class Meta(DetailSerializerV2.Meta):
         model = BadgeClass
+
+
+class BaseRequestIframeSerializer(serializers.Serializer):
+    """Base serializer for all iFrame request endpoints"""
+
+    LANGUAGES = [
+        ("en", "English"),
+        ("de", "German"),
+    ]
+
+    lang = serializers.ChoiceField(choices=LANGUAGES, default="en")
+
+
+class RequestIframeSerializer(serializers.Serializer):
+    email = serializers.CharField()
+
+
+class RequestIframeBadgeProcessSerializer(BaseRequestIframeSerializer):
+    issuer = serializers.CharField(allow_blank=True)
+    badge = serializers.CharField(allow_blank=True)
