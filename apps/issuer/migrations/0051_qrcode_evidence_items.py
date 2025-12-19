6b6c2d0a951b6d5d78e2fb003c249c50a56bc133
jona159 <raabejonas@yahoo.de>
2025-12-19 15:09:22 +0100

    fix: migration order

--- apps/issuer/migrations/0051_qrcode_evidence_items.py
+++ apps/issuer/migrations/0051_qrcode_evidence_items.py
@@ -5,15 +5,14 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('issuer', '0047_rename_badgeinstance_recipient_identifier_badgeclass_revoked_issuer_badg_recipie_6a2cd8_idx_and_more'),
+        ("issuer", "0050_qrcode_course_url"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='qrcode',
-            name='evidence_items',
+            model_name="qrcode",
+            name="evidence_items",
             field=jsonfield.fields.JSONField(blank=True, default=list),
         ),
     ]
